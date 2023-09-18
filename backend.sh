f2d68ba6a77df3deb02ce41c55fc85840138c524
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-09-18 10:16:28 +0530

    test4

--- backend.sh
+++ backend.sh
@@ -15,4 +15,4 @@ systemctl enable backend
 systemctl start backend
 
 dnf install mysql -y      
-mysql -h 172.31.31.237 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.20.190 -uroot -pExpenseApp@1 < /app/schema/backend.sql
