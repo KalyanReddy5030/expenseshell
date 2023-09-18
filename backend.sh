7a1b80d78f063f30250bc6513dc3b5df6f6a834f
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-09-18 16:39:11 +0530

    test4

--- backend.sh
+++ backend.sh
@@ -1,7 +1,11 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+rm -rf /etc/systemd/system/backend.service
 dnf install nodejs -y
 cp backend.service /etc/systemd/system/backend.service
 useradd expense
+
+rm-rf /app
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 
@@ -15,4 +19,4 @@ systemctl enable backend
 systemctl start backend
 
 dnf install mysql -y      
-mysql -h 172.31.20.190 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.20.104 -uroot -pExpenseApp@1 < /app/schema/backend.sql
