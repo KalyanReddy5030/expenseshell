68ac1a43ce7cd969b973bf67b57cac338d769afc
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-09-29 08:09:40 +0530

    test4

--- frontend.sh
+++ frontend.sh
@@ -8,7 +8,7 @@ echo Removing old content
 rm -rf /usr/share/nginx/html/*
 
 echo downloading frontend code
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>/tmp/expense.log
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>/tmp/expense.log
 
 
 cd /usr/share/nginx/html
