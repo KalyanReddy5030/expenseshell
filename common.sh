27d9ee47581366ace7c4b89d54f37c7f003181e4
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-09-29 17:54:36 +0530

    test4

--- common.sh
+++ common.sh
@@ -11,12 +11,12 @@ download_and_extract(){
   #frontend.sh as component=frontend and to execute we give as $component
 echo downloading $component code
 curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >>$log_file
-echo $?
+stat_check
 
 echo extracting $component code
 unzip /tmp/$component.zip >>$log_file
 
-echo $?
+stat_check
 }
 
 stat_check(){
